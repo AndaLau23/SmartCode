cd15b1cc800b597f96da71b7537f40b18ff94d1f
AndaLau23 <908656706@qq.com>
2018-02-02 22:44:51 +0800

    添加LOG和TRACE宏定义

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+#define SmartLog(format, ...)    printf("SmartLife " format "\r\n", ##__VA_ARGS__)
+#define SmartTrace(format, ...)  printf("SmartLife [%s(%d)] " format "\r\n", __FILE__, __LINE__, ##__VA_ARGS__)
+
 void main(void){
      while( true ){
-         printf("Hello World\r\n")；
+         SmartLog("Hello World");
+         SmartTrace("Debug Trace The Code");
      }
 }
